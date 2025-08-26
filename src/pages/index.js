7ab7e49478f6dbb5255d71da4ff1744c97021551
kungjk <jerker@kenntoft.se>
2025-08-26 10:41:07 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,10 @@ const IndexPage = () => (
             Strangely enough they havent responded to emails or phone calls so I decided to rebuild the site with a new, faster, cheaper platform. <br />
             For those of you of a technical interest this means that the site will be running staticly with gatsbyJS under the hood rather than a node.js + mongoDB + bootstrap3 solution. <br />
             With that said, i hope you bear with me as I continue to rebuild the site. <br />
-            Start using the site by clicking on the menu on the upper left, or go directly to <a href="https://www.boardgame-news.com/news/">news</a> or <a href="https://www.boardgame-news.com/reviews/">reviews</a> right here. <br />
-            
+            Start using the site by clicking on the menu on the upper left, or go directly to <a href="https://www.boardgame-news.com/news/">news</a> or <a href="https://www.boardgame-news.com/reviews/">reviews</a> right here. <br /><br />
+
+            We also have a section of toplists which can be found the in the menu, or you could jump directly to the <a href="https://www.boardgame-news.com/top/the-best-marvel-legendary-expansions">Best Marvel: Legendary Expansion (top list)</a>
+            <br />
             Cheers, <br />
             Jay
           
